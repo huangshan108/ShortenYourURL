53a72a8dab3d1970a2832f01a9596af636e915c1
huangshan108 <shanhuang108@gmail.com>
2015-03-18 17:47:59 -0700

    algorithm updated to avoid overflow

--- app/controllers/concerns/cantor_pair.rb
+++ app/controllers/concerns/cantor_pair.rb
@@ -1,11 +1,14 @@
 class CantorPair
 	# 36 bits in total
-	# bit 35: 0 (avoid overflow)
-	# bits 34 - 32: rand(7) (undecipherable)
-	# bits 31 - 16: upper 16 bits of id
+	# bit 35 - 34: 0 (avoid overflow)
+	# bits 33 - 31: random num 0 - 7 (undecipherable)
+	# bits 30 - 16: upper 15 bits of id
 	# bits 15 - 0: lower 16 bits of id
 	def self.encode id
-		upper = (rand(7) << 16) + (id >> 16)
+		if id > (2 ** 31 - 1)
+			raise OverflowError
+		end
+		upper = (rand(8) << 15) + (id >> 16)
 		lower = id & (2 ** 16 - 1)
 		return ((upper + lower) * (upper + lower + 1)) / 2 + lower
 	end
@@ -14,7 +17,7 @@ class CantorPair
 		temp = ((-1 + Math.sqrt(1 + 8 * num)) / 2).floor
 		upper = temp * (temp + 3) / 2 - num
 		lower = num - temp * (temp + 1) / 2
-		return ((upper << 16) + lower) & (2 ** 32 - 1)
+		return ((upper << 16) + lower) & (2 ** 31 - 1)
 	end
 
 end
